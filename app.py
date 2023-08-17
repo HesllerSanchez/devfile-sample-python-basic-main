11a1b8acb9f0a0a2bffd026264b4047b929e98db
HesllerSanchez <HesllerSanchez@gmail.com>
2023-08-17 15:11:25 -0600

    agg html

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 import os
+#import database as db
 
-app = Flask(__name__)
+template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+template_dir = os.path.join(template_dir, 'src', 'templates')
 
+app = Flask(__name__, template_folder = template_dir)
+
+#Rutas de la aplicación
 @app.route('/')
-def hello():
-    return "Hello World!"
-
+def home():
+    return render_template('index.html')
+    
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
